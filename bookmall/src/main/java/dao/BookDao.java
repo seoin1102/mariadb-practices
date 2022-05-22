7212ea03813f893fdb7b8974fd94af39fe2a5fc4
seoin1102 <69146576+seoin1102@users.noreply.github.com>
2022-05-22 17:09:14 +0900

    Update BookDao.java
    필요없는 코드 삭제

--- bookmall/src/main/java/dao/BookDao.java
+++ bookmall/src/main/java/dao/BookDao.java
@@ -46,48 +46,9 @@ public class BookDao {
 		return result;
 	}
 	
-	public void update(Long no, Long price) {
-		BookVo vo = new BookVo();
-		vo.setNo(no);
-		vo.setPrice(price);
-		
-		update(vo);
-	}
-	
-	public boolean update(BookVo vo) {
-		boolean result = false;
-		Connection connection = null;
-		PreparedStatement pstmt = null;
-		
-		try {
-			connection = getConnection();
-			
-			String sql = "update book set state_code= ? where no = ?"; 
-			pstmt = connection.prepareStatement(sql);
-			
-			pstmt.setLong(1, vo.getPrice());
-			pstmt.setLong(2, vo.getNo());
-			
-			int count = pstmt.executeUpdate();
-			result = count == 1;
-		} catch (SQLException e) {
-			System.out.println("드라이버 로딩 실패:" + e);
-		} finally {
-			try {
-				if(pstmt != null) {
-					pstmt.close();
-				}
-				if(connection != null) {
-					connection.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		
-		return result;
-	}
 
+	
+	
 	public List<BookVo> findAll() {
 		List<BookVo> result = new ArrayList<>();
 		Connection connection = null;
@@ -99,13 +60,13 @@ public class BookDao {
 			String sql = "select a.no, a.title, a.price, b.name\r\n"
 					+ "from book a, category b\r\n"
 					+ "where a.category_no = b.no\r\n"
-					+ "order by a.no;";
+					+ "order by a.no";
 			pstmt = connection.prepareStatement(sql);
 			
 			rs = pstmt.executeQuery();
 
 			while(rs.next()) {
-				Long no = rs.getLong(1); // db 1로 시작
+				Long no = rs.getLong(1); 
 				String title = rs.getString(2);
 				Long price = rs.getLong(3);
 				String categoryName = rs.getString(4);
@@ -138,60 +99,18 @@ public class BookDao {
 		return result;
 	}
 	
-	public BookVo findByNo(long no) {
-		BookVo result = null;
-		Connection connection = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		try {
-			connection = getConnection();
-			
-			String sql = "select a.no, a.title, a.author_no, b.name, a.state_code\r\n"
-					+ "	from book a, author b\r\n"
-					+ "where a.author_no = b.no\r\n"
-					+ "and a.no = ?\r\n"
-					+ "order by a.no asc";
-			pstmt = connection.prepareStatement(sql);
-			pstmt.setLong(1, no);
-			rs = pstmt.executeQuery();
 
-			if(rs.next()) {
-				result = new BookVo();
-				
-				result.setNo(rs.getLong(1)); 
-				result.setTitle(rs.getString(2));
-				result.setPrice(rs.getLong(3));
-				result.setCategoryNo(rs.getLong(4));
-			}
-		} catch (SQLException e) {
-			System.out.println("드라이버 로딩 실패:" + e);
-		} finally {
-			try {
-				if(rs != null) {
-					rs.close();
-				}
-				if(pstmt != null) {
-					pstmt.close();
-				}
-				if(connection != null) {
-					connection.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		return result;
-	}
+
 	
 	private Connection getConnection() throws SQLException {
 		Connection connection = null;
 		try {
 			Class.forName("org.mariadb.jdbc.Driver");
 			
-			String url = "jdbc:mysql://192.168.10.36:3306/bookmall?charset=utf8";
+			String url = "jdbc:mysql://192.168.35.179:3306/bookmall?charset=utf8";
 			connection = DriverManager.getConnection(url, "bookmall", "bookmall");
 		} catch (ClassNotFoundException e) {
-			System.out.println("드라이버 로딩 실패"+ e);
+			System.out.println("ERROR:"+ e);
 		}
 		return connection;
 	}
