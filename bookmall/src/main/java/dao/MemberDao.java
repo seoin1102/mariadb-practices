9aad0255503f79dd950fb02952bbd57977bec1bf
seoin1102 <69146576+seoin1102@users.noreply.github.com>
2022-05-22 17:14:41 +0900

    필요없는 코드 삭제
    delete 기능을 추가하려했으나 실패...

--- bookmall/src/main/java/dao/MemberDao.java
+++ bookmall/src/main/java/dao/MemberDao.java
@@ -45,49 +45,6 @@ public class MemberDao {
 		return result;
 	}
 	
-	public void update(Long no, String name, String email) {
-		MemberVo vo = new MemberVo();
-		vo.setNo(no);
-		vo.setName(name);
-		vo.setEmail(email);
-		
-		update(vo);
-	}
-	
-	public boolean update(MemberVo vo) {
-		boolean result = false;
-		Connection connection = null;
-		PreparedStatement pstmt = null;
-		
-		try {
-			connection = getConnection();
-			
-			String sql = "update member set name = ?, email = ? where no = 1;"; 
-			pstmt = connection.prepareStatement(sql);
-			
-			pstmt.setString(1, vo.getName());
-			pstmt.setString(2, vo.getEmail());
-			
-			int count = pstmt.executeUpdate();
-			result = count == 1;
-		} catch (SQLException e) {
-			System.out.println("드라이버 로딩 실패:" + e);
-		} finally {
-			try {
-				if(pstmt != null) {
-					pstmt.close();
-				}
-				if(connection != null) {
-					connection.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		
-		return result;
-	}
-
 	public List<MemberVo> findAll() {
 		List<MemberVo> result = new ArrayList<>();
 		Connection connection = null;
@@ -102,7 +59,7 @@ public class MemberDao {
 			rs = pstmt.executeQuery();
 
 			while(rs.next()) {
-				Long no = rs.getLong(1); // db 1로 시작
+				Long no = rs.getLong(1); // db 1濡� ����
 				String name = rs.getString(2);
 				int phone = rs.getInt(3);
 				String email = rs.getString(4);
@@ -138,57 +95,16 @@ public class MemberDao {
 		return result;
 	}
 	
-	public MemberVo findByNo(long no) {
-		MemberVo result = null;
-		Connection connection = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		try {
-			connection = getConnection();
-			
-			String sql = "select name, email from member where no = ?";
-			pstmt = connection.prepareStatement(sql);
-			pstmt.setLong(1, no);
-			rs = pstmt.executeQuery();
-
-			if(rs.next()) {
-				result = new MemberVo();
-				
-				result.setNo(rs.getLong(1)); 
-				result.setName(rs.getString(2));
-				result.setPhone(rs.getInt(3));
-				result.setEmail(rs.getString(4));
-				result.setPassword(rs.getString(5));
-			}
-		} catch (SQLException e) {
-			System.out.println("드라이버 로딩 실패:" + e);
-		} finally {
-			try {
-				if(rs != null) {
-					rs.close();
-				}
-				if(pstmt != null) {
-					pstmt.close();
-				}
-				if(connection != null) {
-					connection.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		return result;
-	}
 	
 	private Connection getConnection() throws SQLException {
 		Connection connection = null;
 		try {
 			Class.forName("org.mariadb.jdbc.Driver");
 			
-			String url = "jdbc:mysql://192.168.10.36:3306/bookmall?charset=utf8";
+			String url = "jdbc:mysql://192.168.10.46:3306/bookmall?charset=utf8";
 			connection = DriverManager.getConnection(url, "bookmall", "bookmall");
 		} catch (ClassNotFoundException e) {
-			System.out.println("드라이버 로딩 실패"+ e);
+			System.out.println("ERROR"+ e);
 		}
 		return connection;
 	}
