802d0add086bcecdc43e29c5adfd25797f2b005c
seoin1102 <69146576+seoin1102@users.noreply.github.com>
2022-05-22 17:59:18 +0900

    Update BookMall.java

--- bookmall/src/main/java/main/BookMall.java
+++ bookmall/src/main/java/main/BookMall.java
@@ -25,76 +25,77 @@ public class BookMall {
 
 	}
 	private static void insertData() {
-//		CategoryDao categorydao = new CategoryDao();
-//		CategoryVo categoryvo = new CategoryVo();
-//		categoryvo.setName("과학");
-//		categorydao.insert(categoryvo);
-//		
-//		categoryvo.setName("IT");
-//		categorydao.insert(categoryvo);
-//	
-//		categoryvo.setName("문학");
-//		categorydao.insert(categoryvo);
+CategoryDao categorydao = new CategoryDao();
+		CategoryVo categoryvo = new CategoryVo();
+		categoryvo.setName("과학");
+		categorydao.insert(categoryvo);
 		
-//		BookDao bookDao = new BookDao();
-//		BookVo bookVo = new BookVo();
-//		bookVo.setTitle("이것이 MariaDB다");
-//		bookVo.setPrice(1000L);
-//		bookVo.setCategoryNo(5L);
-//		bookDao.insert(bookVo);  
-//		
-//		bookVo.setTitle("러닝 Rect.JS");
-//		bookVo.setPrice(2000L);
-//		bookVo.setCategoryNo(5L);
-//		bookDao.insert(bookVo);
-//		
-//		bookVo.setTitle("코스모스");
-//		bookVo.setPrice(1000L);
-//		bookVo.setCategoryNo(4L);
-//		bookDao.insert(bookVo);
-//		
-//		MemberDao memberDao = new MemberDao();
-//		MemberVo memberVo = new MemberVo();
-//		memberVo.setName("둘리");
-//		memberVo.setEmail("dooly@gmail.com");
-//		memberDao.insert(memberVo);
-//		
-//		memberVo.setName("마이콜");
-//		memberVo.setEmail("michol@gmail.com ");
-//		memberDao.insert(memberVo);
-//		
+		categoryvo.setName("IT");
+		categorydao.insert(categoryvo);
+	
+		categoryvo.setName("문학");
+		categorydao.insert(categoryvo);
+		
+		BookDao bookDao = new BookDao();
+		BookVo bookVo = new BookVo();
+		bookVo.setTitle("이것이 MariaDB다");
+		bookVo.setPrice(1000L);
+		bookVo.setCategoryNo(2L);
+		bookDao.insert(bookVo);  
+		
+		bookVo.setTitle("러닝 Rect.JS");
+		bookVo.setPrice(2000L);
+		bookVo.setCategoryNo(2L);
+		bookDao.insert(bookVo);
+		
+		bookVo.setTitle("코스모스");
+		bookVo.setPrice(1000L);
+		bookVo.setCategoryNo(1L);
+		bookDao.insert(bookVo);
+		
+		MemberDao memberDao = new MemberDao();
+		MemberVo memberVo = new MemberVo();
+		memberVo.setName("둘리");
+		memberVo.setEmail("dooly@gmail.com");
+		memberDao.insert(memberVo);
+		
+		memberVo.setName("마이콜");
+		memberVo.setEmail("michol@gmail.com ");
+		memberDao.insert(memberVo);
+		
 		CartDao cartDao = new CartDao();
 		CartVo cartVo = new CartVo();
-//		cartVo.setMemberNo(3L);
-//		cartVo.setBookNo(6L);
-//		cartVo.setStock(2);
-//		cartDao.insert(cartVo);
-//			
-//		cartVo.setMemberNo(4L);
-//		cartVo.setBookNo(6L);
-//		cartVo.setStock(1);
-//		cartDao.insert(cartVo);
-//		
-//		
+		cartVo.setMemberNo(1L);
+		cartVo.setBookNo(1L);
+		cartVo.setStock(2);
+		cartDao.insert(cartVo);
+			
+		cartVo.setMemberNo(1L);
+		cartVo.setBookNo(3L);
+		cartVo.setStock(1);
+		cartDao.insert(cartVo);
+		
+		
 		OrderDao orderDao = new OrderDao();
 		OrderVo orderVo = new OrderVo();
-//		orderVo.setNo(20220519L);
-//		orderVo.setNumber(1L);
-//		orderVo.setMemberNo(3L);
-//		orderVo.setPaymentPrice(3000L);
-//		orderVo.setReceiveAddress("센텀호텔");
-//	    orderDao.insert(orderVo);
+		orderVo.setNo(20220519L);
+		orderVo.setNumber(1L);
+		orderVo.setMemberNo(1L);
+		orderVo.setPaymentPrice(3000L);
+		orderVo.setReceiveAddress("센텀호텔");
+	    orderDao.insert(orderVo);
 
 			
-		orderVo.setBookNo(6L);
-		orderVo.setOrderNo(20220521L);
+		orderVo.setBookNo(1L);
+		orderVo.setOrderNo(20220519L);
 		orderVo.setStock(2L);
 	    orderDao.insertOrderBook(orderVo);
 	    
-	    orderVo.setBookNo(7L);
-	    orderVo.setOrderNo(20220521L);
+	    orderVo.setBookNo(3L);
+	    orderVo.setOrderNo(20220519L);
 	    orderVo.setStock(1L);	    
 	    orderDao.insertOrderBook(orderVo);
+	  
 	  
 		
 		
